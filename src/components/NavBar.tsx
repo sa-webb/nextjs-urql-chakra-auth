982bd3e5d20e2b919b5649421e5934a32d6228f6
sa-webb <sawebb30@gmail.com>
2020-09-04 14:02:32 -0400

    initial

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Box, Flex, Link, Button } from "@chakra-ui/core";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+
+  let display;
+
+  if (fetching) {
+    display = null;
+  } else if (!data?.me) {
+    display = (
+      <>
+        <NextLink href="/register">
+          <Link mr={4}>Register</Link>
+        </NextLink>
+        <NextLink href="/login">
+          <Link>Login</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    display = (
+      <Flex>
+        <Box mr={4}>{data?.me?.username}</Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="rebeccapurple" p={4}>
+      <Box ml={"auto"}></Box>
+      {display}
+    </Flex>
+  );
+};
