bcb3412075a69d1c4cd836307a1861526c01dbad
sa-webb <sawebb30@gmail.com>
2020-09-06 12:24:57 -0400

    username & email

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -17,9 +17,9 @@ const Register: React.FC<RegisterProps> = ({}) => {
   return (
     <Wrapper variant="small">
       <Formik
-        initialValues={{ username: "", password: "" }}
+        initialValues={{ email: "", username: "", password: "" }}
         onSubmit={async (values, { setErrors }) => {
-          const response = await register(values);
+          const response = await register({options: values});
           if (response.data?.register.errors) {
             setErrors(toErrorMap(response.data.register.errors));
           } else if (response.data?.register.user) {
@@ -29,6 +29,13 @@ const Register: React.FC<RegisterProps> = ({}) => {
       >
         {({ isSubmitting }) => (
           <Form>
+            <Box>
+              <InputField
+                name="email"
+                placeholder="email"
+                label="email"
+              />
+            </Box>
             <Box>
               <InputField
                 name="username"
